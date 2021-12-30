f40e507662738bc54fd5b983cf76c0881ccf0a99
redpoint <Baskara Febrianto>
2021-12-30 20:35:03 +0700

    fixing unused export property

--- src/icons/SortDescending.svelte
+++ src/icons/SortDescending.svelte
@@ -8,5 +8,5 @@
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" class={className} {fill} viewBox="0 0 24 24" stroke="currentColor" style="width:{size}rem;height:{size}rem" on:click={() => dispatch('click')}>
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width={strokeWidth} d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
 </svg>
