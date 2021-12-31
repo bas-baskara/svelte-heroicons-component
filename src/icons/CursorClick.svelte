852d2d6d37542f56d834ce35cedbff36ffadd671
redpoint <Baskara Febrianto>
2021-12-31 09:53:29 +0700

    override strokewidth

--- src/icons/CursorClick.svelte
+++ src/icons/CursorClick.svelte
@@ -5,6 +5,8 @@
 
     export let className = "", size = "1.5", strokeWidth = "2", fill = "none"
 
+    if (className.includes('stroke-')) strokeWidth = null
+
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" class={className} {fill} viewBox="0 0 24 24" stroke="currentColor" style="width:{size}rem;height:{size}rem" on:click={() => dispatch('click')}>
