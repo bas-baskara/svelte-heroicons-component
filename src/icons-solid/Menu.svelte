a036db0bc9e4bc99948dc97b73407882725273bf
Baskara Febrianto <baskarafebrianto@gmail.com>
2022-01-01 22:23:31 +0700

    add solid icons

--- src/icons-solid/Menu.svelte
+++ src/icons-solid/Menu.svelte
@@ -17,6 +17,7 @@
     }
 
 </script>
-<svg xmlns="http://www.w3.org/2000/svg" class={className} {fill} viewBox="0 0 24 24" stroke="currentColor" style="width:{size}rem;height:{size}rem;" on:click={() => dispatch('click')}>
+
+<svg xmlns="http://www.w3.org/2000/svg" class={className} viewBox="0 0 20 20" fill="currentColor" style="width:{size}rem;height:{size}rem" on:click={() => dispatch('click')}>
     <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
 </svg>
